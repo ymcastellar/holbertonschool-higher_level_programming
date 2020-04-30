0e3a45f63fe2ba7f69232a1a48270fc3b18d4c63
ymcastellar <yoycas@hotmail.com>
2020-04-30 17:03:51 -0500

    fixed file for task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if name == "main":
+if __name__ == "__main__":
     from hidden_4 import *
-
-for i in dir():
-    if i[:2] != "__":
-        print(i)
+    for s in dir():
+        if s[:2] != '__':
+            print(s)
