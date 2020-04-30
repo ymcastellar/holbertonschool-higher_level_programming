a37b98f1afda95ee1f4a168636e8a38028cc66b7
ymcastellar <yoycas@hotmail.com>
2020-04-30 15:29:45 -0500

    fixed file for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arg = len(sys.argv)
-    if arg > 1:
-        print("{} arguments".format(arg))
-    else:
-        print("{} argument".format(arg))
+arg = len(sys.argv) - 1
+name = sys.argv
+if arg == 0:
+    print("{} arguments.".format(arg))
+elif arg == 1:
+    print("{} argument:".format(arg))
+    for i in range(arg):
+        i = i + 1
+        print("{}: {}".format(i, name[i]))
+else:
+    print("{} arguments:".format(arg))
+    for i in range(arg):
+        i = i + 1
+        print("{}: {}".format(i, name[i]))
