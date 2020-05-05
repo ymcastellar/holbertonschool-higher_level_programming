98221ca11bf56d946421b43fe3c0c4007cf7ecd9
ymcastellar <yoycas@hotmail.com>
2020-05-05 16:18:43 -0500

    fixed indent

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        my_string = list(my_string)
+        new_list = list(my_string)
         for i in new_list:
             if i in "Cc":
                 new_list.remove(i)
