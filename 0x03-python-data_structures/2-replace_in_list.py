ea2d1f649090e90d49f0aa4bf7d1f7904ca97d55
ymcastellar <yoycas@hotmail.com>
2020-05-05 14:26:38 -0500

    modif line

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,5 +5,5 @@ def replace_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return(my_list)
     else:
-        my_list[3] = element
+        my_list[idx] = element
         return(my_list)
