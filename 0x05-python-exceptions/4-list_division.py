f0647b74ab439dad80f41c595314e71385565129
ymcastellar <yoycas@hotmail.com>
2020-05-18 16:34:58 -0500

    added files for task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    res = 0
+    newlist = []
+    for i in range(list_length):
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            newlist.append(res)
+            res = 0
+    return newlist
