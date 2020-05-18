dbcb2a239c77040c5b9bb9ba3039ecb184ccecb0
ymcastellar <yoycas@hotmail.com>
2020-05-18 16:01:16 -0500

    added files for task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    res = None
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(res))
+        return res
