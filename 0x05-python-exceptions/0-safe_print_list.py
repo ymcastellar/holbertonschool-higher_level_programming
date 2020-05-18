8275ae015b60ad55dd71b631464259190c1e14e4
ymcastellar <yoycas@hotmail.com>
2020-05-18 14:25:46 -0500

    fix pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
-
 #!/usr/bin/python3
+
 
 def safe_print_list(my_list=[], x=0):
     con = 0
     for i in range(x):
         try:
-           print(my_list[i], end='')
-           con = con + 1
+            print(my_list[i], end='')
+            con = con + 1
         except IndexError:
             break
     print()
