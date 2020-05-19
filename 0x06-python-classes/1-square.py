09e413d985509a2e0c5cd25ba91c8e12a586a6b0
ymcastellar <yoycas@hotmail.com>
2020-05-19 14:35:46 -0500

    added files for task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """ class that defines a square """
+    def __init__(self, size):
+        """ ---builder to define size---
+
+        Args: size: size of square.
+
+        """
+
+        self.__size = size
