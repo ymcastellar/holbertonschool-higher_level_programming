b6bd64b2d06d7b6a50edd470a9e0a810f99e6c51
ymcastellar <yoycas@hotmail.com>
2020-05-19 17:03:29 -0500

    added files for task3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" class that defines a square """
+
+
+class Square:
+    """ ---builder to define size---
+
+        Args: size: size of square.
+
+        """
+    def __init__(self, size=0):
+        self.__size = size
+
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    """area - return the the square area"""
+
+    def area(self):
+        return (self.__size ** 2)
