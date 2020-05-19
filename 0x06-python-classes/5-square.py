59b52965ea803d96eef2449c2748a7814cdfa3eb
ymcastellar <yoycas@hotmail.com>
2020-05-19 18:13:08 -0500

    added files for task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" class that defines a square """
+
+
+class Square:
+    """ ---builder to define size---
+
+        Args: size: size of square.
+
+        """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    """area - return the the square area"""
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        list = []
+        for i in range(self.__size):
+            list.append("#" * self.__size)
+        print(*list, sep="\n")
