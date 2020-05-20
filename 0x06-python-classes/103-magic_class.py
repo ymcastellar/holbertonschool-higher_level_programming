03fe683fa4132f853a98eb811c057d995e12b416
ymcastellar <yoycas@hotmail.com>
2020-05-20 16:19:28 -0500

    fixing pep8

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 import math
-
-
-
-
+"""magig class"""
 
 
 class MagicClass:
+    """calculate area and circumference"""
+
     def __init__(self, radius=0):
+        self.radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.___radius = radius
 
-
     def area(self):
         return (self.__radius ** 2) * math.pi
-
 
     def circumference(self):
         return 2 * math.pi * self.__radius
