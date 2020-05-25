1814c4069fa1af17cca1da1d2856cb623f7239c5
ymcastellar <yoycas@hotmail.com>
2020-05-25 14:12:17 -0500

    fixed parameter conditions

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -37,6 +37,9 @@ class Rectangle:
 
     def area(self):
         return (self.__height * self.__width)
-    
+
     def perimeter(self):
-        return ((self.__height * 2) + (self.__width * 2))
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return ((self.__height * 2) + (self.__width * 2))
