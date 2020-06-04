1b2705170d31ed92325b2ddcdd22ebe2ddaad9f0
ymcastellar <yoycas@hotmail.com>
2020-06-03 23:02:17 -0500

    added files for task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""[Student to JSON with filter]
+"""
+
+
+class Student:
+    """[Student]
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dic = {}
+        if attrs is None:
+            return self.__dict__
+        for i in attrs:
+            if hasattr(self, i):
+                dic[i] = getattr(self, i)
+        return dic
