6ab0fb581c72df550c561a923ff94fc0001ee1a7
ymcastellar <yoycas@hotmail.com>
2020-06-09 14:15:11 -0500

    fixed conditions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -60,7 +60,7 @@ class Rectangle(Base):
         if type(x) is not int:
             raise TypeError("x must be an integer")
         if x < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -83,7 +83,7 @@ class Rectangle(Base):
         if type(y) is not int:
             raise TypeError("y must be an integer")
         if y < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = y
 
     def area(self):
