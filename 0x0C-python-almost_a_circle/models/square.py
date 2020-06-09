3a6e74276462757798af7775360ec03534e4df30
ymcastellar <yoycas@hotmail.com>
2020-06-09 14:25:17 -0500

    fixed pep8

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,7 +13,7 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ stdout 
+        """ stdout
         """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
