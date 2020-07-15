36247227299d6fa631bf6bad226d8c72bad77e9a
ymcastellar <yoycas@hotmail.com>
2020-07-14 22:41:29 -0500

    added fiel for task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+
+SELECT tv_shows.title, tv_genres.name FROM tv_show_genres LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_genres.name = 'Comedy'  ORDER BY tv_shows.title, tv_genres.name ;
