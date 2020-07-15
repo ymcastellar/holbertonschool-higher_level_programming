49d157d927fbe3893cb4714ab8ce545a9cdb1ad4
ymcastellar <yoycas@hotmail.com>
2020-07-14 20:53:11 -0500

    added ASC

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
 
-SELECT cities.id, cities.name, states.name FROM cities join states ON states.id = state_id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities join states ON states.id = state_id ORDER BY cities.id ASC;
