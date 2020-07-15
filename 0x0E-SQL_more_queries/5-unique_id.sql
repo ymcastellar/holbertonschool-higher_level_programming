d0ce905df5fca67ca007fc6915ea74744902027d
ymcastellar <yoycas@hotmail.com>
2020-07-14 19:35:57 -0500

    added files for task

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- script that creates the table unique_id on your MySQL server
+
+CREATE TABLE IF NOT EXISTS unique_id  (
+    id INT UNIQUE DEFAULT 1, 
+    name VARCHAR(256),
+    PRIMARY KEY (id)
+);
