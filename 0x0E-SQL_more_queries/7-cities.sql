91edaaec8a99a737ccc00680a44590b01a29ad72
ymcastellar <yoycas@hotmail.com>
2020-07-14 19:51:06 -0500

    added a comma

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL UNIQUE AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
