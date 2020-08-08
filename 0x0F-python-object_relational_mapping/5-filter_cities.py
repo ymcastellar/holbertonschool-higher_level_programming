e1eeb2fe5c524f4fd1438a3055c2df6930802ea8
ymcastellar <yoycas@hotmail.com>
2020-08-08 13:55:09 -0500

    fixed lines too lng

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in the name of a state as an argument and 
+"""script that takes in the name of a state as an argument and
    lists all cities of that state, using the database hbtn_0e_4_usa
 """
 import MySQLdb
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states ON state_id = states.id WHERE states.name = %(sql_name)s ORDER BY cities.id ASC", {'sql_name': sys.argv[4]})
+    cur.execute("""SELECT cities.name FROM cities JOIN states ON state_id = states.id
+                WHERE states.name = %(sql_name)s ORDER BY cities.id ASC""",
+                {'sql_name': sys.argv[4]})
     rows = cur.fetchall()
     new_list = []
     for row in rows:
