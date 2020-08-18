d44d18eff88bbe3ac3bdd95d40696653cffeb366
ymcastellar <yoycas@hotmail.com>
2020-08-18 15:51:10 -0500

    fix coments

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# script that sends a DELETE request to the URL
 curl -sX DELETE "$1"
