2b61bd88423c245441657158b3218da64fe94064
ymcastellar <yoycas@hotmail.com>
2020-08-18 19:09:45 -0500

    fixing code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,11 +6,22 @@ def find_peak(list_of_integers):
     """find a peak in a list"""
 
     if list_of_integers:
-        var = list_of_integers[0]
-        for i in range(1, len(list_of_integers) - 1):
-            if (list_of_integers[i] > list_of_integers[i - 1]
-                    and list_of_integers[i] > list_of_integers[i + 1]):
-                    var = list_of_integers[i]
-        return var
+
+        length = len(list_of_integers)
+
+        if length == 1:
+            return list_of_integers[0]
+        elif length == 2:
+            return max(list_of_integers)
+
+        mid = int(length / 2)
+
+        if (list_of_integers[mid] > list_of_integers[mid - 1]
+                and list_of_integers[mid] > list_of_integers[mid + 1]):
+                    return list_of_integers[mid]
+        elif list_of_integers[mid] < list_of_integers[mid - 1]:
+            return find_peak(list_of_integers[:mid])
+        else:
+            return find_peak(list_of_integers[mid + 1:])
     else:
         return None
