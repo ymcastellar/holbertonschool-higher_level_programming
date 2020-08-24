7cdc53a8751741084c6abaf761706defa5325de8
ymcastellar <yoycas@hotmail.com>
2020-08-24 18:23:27 -0500

    dict fixed

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    data = urllib.parse.urlencode({"email": sys.argv[2]})
+    data = {"email": sys.argv[2]}
     response = request.post(sys.argv[1], data)
     print(response.text)
