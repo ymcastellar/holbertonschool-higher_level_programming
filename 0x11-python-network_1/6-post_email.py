f5cb2edfa0399c344541350baa32098c01315098
ymcastellar <yoycas@hotmail.com>
2020-08-24 18:22:20 -0500

    coding fixed

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,5 @@ import sys
 
 if __name__ == "__main__":
     data = urllib.parse.urlencode({"email": sys.argv[2]})
-    data = data.encode('ascii')
-
     response = request.post(sys.argv[1], data)
     print(response.text)
