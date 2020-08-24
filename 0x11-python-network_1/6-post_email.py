35a5bba7324aa9f79a876048124d7a2a9c3fe769
ymcastellar <yoycas@hotmail.com>
2020-08-24 18:27:27 -0500

    requests fixed

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ import sys
 
 if __name__ == "__main__":
     obj = {"email": sys.argv[2]}
-    response = request.post(sys.argv[1], data=obj)
+    response = requests.post(sys.argv[1], data=obj)
     print(response.text)
