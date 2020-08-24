a350f8329d882da6cc1033ff27e8437a854f86e2
ymcastellar <yoycas@hotmail.com>
2020-08-24 15:18:37 -0500

    main condition added

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,11 @@
 
 import urllib.request
 
-req = urllib.request.Request('https://intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
-    print('Body response:')
-    print('\t- type: {} '.format(type(the_page)))
-    print('\t- content: {}'.format(the_page))
-    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
+if __name__ == "__main__":
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print('Body response:')
+        print('\t- type: {} '.format(type(the_page)))
+        print('\t- content: {}'.format(the_page))
+        print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
