d943b87f72621d0138c89099f33311ef04beb9f0
ymcastellar <yoycas@hotmail.com>
2020-08-24 16:59:52 -0500

    encode fixing

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     data = urllib.parse.urlencode({"email": sys.argv[2]})
-    data = data.encode('utf-8')
+    data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
