65fd2d2af26283040367f9ddd1a6eebb95a215b3
ymcastellar <yoycas@hotmail.com>
2020-08-24 18:20:12 -0500

    file for task 6 added

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     data = urllib.parse.urlencode({"email": sys.argv[2]})
     data = data.encode('ascii')
 
-    response = urllib.request.urlopen(sys.argv[1], data)
+    response = request.post(sys.argv[1], data)
     print(response.text)
