1f62fa0f614b8dcd56d8a3c9ce1d7b908ab66ee8
ymcastellar <yoycas@hotmail.com>
2020-08-24 18:12:58 -0500

    file for task 5 added

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""displays the value of the variable X-Request-Id"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+    print(dict(req.headers).get('X-Request-Id'))
