4e13d40b2f91331cbce995649fde121ee5e220bd
ymcastellar <yoycas@hotmail.com>
2020-09-15 19:04:33 -0500

    fix semi

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
 function factorialize (num) {
-  // if num don't exist
-  if (typeof num !== 'undefined') return 1;
-  // If the number is less than 0, reject it.
   if (num < 0) return -1;
-  // If the number is 0, its factorial is 1.
-  else if (num === 0) return 1;
+  // If the number is 0 or if num doesn't exist, its factorial is 1.
+  else if (num === 0 || isNaN(num)) return 1;
   // Otherwise, call the recursive procedure again
   else {
     return num * factorialize(num - 1);
