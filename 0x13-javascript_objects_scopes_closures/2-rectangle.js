3f560810efaf1e5d215b539e9430154983790090
ymcastellar <yoycas@hotmail.com>
2020-09-21 12:13:38 -0500

    fix order and semi

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(h, w) {
+  constructor (w, h) {
     if (h > 0 && w > 0) {
-      this.height = h;
       this.width = w;
+      this.height = h;
     }
   }
 }
